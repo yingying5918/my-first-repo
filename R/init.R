5059c68fd7ce6006922840bb2079d45300478807
yingying5918 <yingywen@gmail.com>
2019-10-11 01:40:12 +0000

    first commit

--- R/init.R
+++ R/init.R
@@ -1,8 +1,8 @@
-user_name = "kirill"
-user_email = "kirill@gmail.com"
+user_name = "yingying5918"
+user_email = "yingywen@gmail.com"
 
-cmd1 <- paste0("git config --global user.name ", user_name)
-cmd2 <- paste0("git config --global user.email ", user_email)
+cmd1 <- paste0("git config --global user.name", user_name)
+cmd2 <- paste0("git config --global user.email", user_email)
 cmd3 <- paste0("git config --global --list")
 # this is 5 hours
 cmd4 <- paste0("git config credential.helper 'cache --timeout=18000'")
